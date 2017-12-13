c1884808cb968197e7f8793887bb09f84f61fe4c
Lars Petri Christensen <larsc15@student.sdu.dk>
2017-12-13 12:44:08 +0100

    Added correct class library, that is not a solution in it self.

---
+++ PrizeDrawingLibrary/KeyGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrizeDrawingLibrary
+{
+    public class KeyGenerator
+    {
+        private List<string> keys;
+
+        /// <summary>
+        /// Initializes the list of strings.
+        /// </summary>
+        public KeyGenerator()
+        {
+            keys = new List<string>();
+        }
+
+        /// <summary>
+        /// Generates 100 Guids, deletes symbols that accur too much.
+        /// </summary>
+        /// <returns>the generated keys</returns>
+        public List<string> GenerateKeys()
+        {
+            try
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Guid g = Guid.NewGuid();
+                    string GuidString = Convert.ToBase64String(g.ToByteArray());
+                    GuidString = GuidString.Replace("=", "");
+                    GuidString = GuidString.Replace("+", "");
+                    GuidString = GuidString.Replace("/", "");
+
+                    keys.Add(GuidString);
+
+                }
+            }
+            catch (Exception e)
+            {
+
+                Debug.Write(e);
+            }
+            return keys;
+        }
+    }
+}
---
+++ PrizeDrawingLibrary/ValidateUserInput.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrizeDrawingLibrary
+{
+    public class ValidateUserInput
+    {
+        WriterReader writerReader = new WriterReader();
+
+
+        /// <summary>
+        /// check if a string is empty
+        /// </summary>
+        /// <param name="str">the string to check</param>
+        /// <returns>false if empty, else true</returns>
+        public bool CheckIfFirstNameIsValid(string str)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(str))
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+
+                Debug.Write(e);
+            }
+            return true;
+        }
+        /// <summary>
+        /// check if a string is empty
+        /// </summary>
+        /// <param name="str">the string to check</param>
+        /// <returns>false if empty, else true</returns>
+        public bool CheckIfSurNameIsValid(string str)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(str))
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+
+                Debug.Write(e);
+            }
+            return true;
+        }
+        /// <summary>
+        /// checks if an string is a valid email using EmailAddressAttribute.
+        /// </summary>
+        /// <param name="str">the string to check</param>
+        /// <returns>false if not a correct email, else true</returns>
+        public bool CheckIfEmailIsValid(string str)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(str))
+                {
+                    return false;
+                }
+                else if (!new EmailAddressAttribute().IsValid(str))
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+
+                Debug.Write(e);
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// checks if a string is a number
+        /// </summary>
+        /// <param name="str">the string to check</param>
+        /// <returns>false if empty or not a number, else true</returns>
+        public bool CheckIfPhoneNumberIsValid(string str)
+        {
+            try
+            {
+                var isNumeric = int.TryParse(str, out var n);
+                if (string.IsNullOrEmpty(str))
+                {
+                    return false;
+                }
+                if (!isNumeric)
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+
+                Debug.Write(e);
+            }
+            return true;
+        }
+        /// <summary>
+        /// checks if a string is a valid date, follows YYYY-MM-DD.
+        /// </summary>
+        /// <param name="str">the string to check</param>
+        /// <returns>false if empty or not a valid date, else true </returns>
+        public bool CheckIfDateOfBirthIsValid(string str)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(str))
+                {
+                    return false;
+                }
+                else if (!DateTime.TryParse(str, out DateTime dateTime))
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+
+                Debug.Write(e);
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// checks if a string is a valid key in the generated key list.
+        /// </summary>
+        /// <param name="keystring">the string to check</param>
+        /// <param name="keys">the list of generated keys</param>
+        /// <returns>false if keystring not in list, else true</returns>
+        public bool CheckIfSerialIsValid(string keystring, List<string> keys)
+        {
+            try
+            {
+                foreach (String key in keys)
+                {
+                    if (keystring == key)
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+                Debug.Write(e);
+            }
+            return false;
+        }
+
+    }
+
+}
